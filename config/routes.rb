d0809ad05219d325e33e37a4353465aa4b6f0b65
ltude <ltude12@gmail.com>
2014-05-01 16:49:18 -0700

    hm7routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ App::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'static_pages#home'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
